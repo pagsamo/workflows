75a15abc44f014f028c4e227f51f613859837f41
Christian Mark Bituin <bituin.chrismark@gmail.com>
2015-11-26 19:36:30 +0800

    Minified our HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
 	connect = require('gulp-connect'),
 	gulpif = require('gulp-if'),
 	uglify = require('gulp-uglify'),
+	minifyHTML = require('gulp-minify-html'),
 	concat = require('gulp-concat');
 
 var env, coffeeSources, jsSources, sassSources, htmlSources, jsonSources, outputDir, sassStyle;
@@ -58,7 +59,9 @@ gulp.task('compass', function(){
 });
 
 gulp.task('html', function(){
-	gulp.src(htmlSources)
+	gulp.src('builds/development/*.html')
+		.pipe(gulpif(env==='production', minifyHTML()))
+		.pipe(gulpif(env==='production', gulp.dest(outputDir)))
 		.pipe(connect.reload());
 });
 
@@ -78,7 +81,7 @@ gulp.task('watch', function(){
 	gulp.watch(coffeeSources,['coffee']);
 	gulp.watch(jsSources,['js']);
 	gulp.watch('components/sass/*.scss',['compass']);
-	gulp.watch(htmlSources,['html']);
+	gulp.watch('builds/development/*.html',['html']);
 	gulp.watch(jsonSources,['json']);
 });
 
